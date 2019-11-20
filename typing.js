d9d20913cda3a17fbdb867580b85f4b3367d341e
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-11-20 15:45:21 +0000

    finished portfolio

--- typing.js
+++ typing.js
@@ -1,1 +1,31 @@
+let typed1 = new Typed('.headerText1', {
+        strings: [
+            '',
+            'Marc Gallagher-Reid\n'
+        ],
+    startDelay: 500,
+    loop: false,
+    typeSpeed: 70
+});
+
+let typed2 = new Typed('.headerText2', {
+    strings: [
+        '',
+        'Full Stack Developer\n'
+    ],
+    startDelay: 3000,
+    loop: false,
+    typeSpeed: 70
+});
+
+let typed3 = new Typed('.headerText3', {
+    strings: [
+        '',
+        'Mayden Academy Graduate\n'
+    ],
+    startDelay: 6000,
+    loop: false,
+    typeSpeed: 70
+});
+
 
