38260b97bf4628e1fbed7a88969a8af7e4bbfb37
Marc <marc.gallagherreid11@bathspa.ac.uk>
2020-05-16 22:21:50 +0100

    added everything

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 
 let style = () => {
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('app/css/'))
+        .pipe(gulp.dest('src/css/'))
         .pipe(browserSync.stream());
 }
 
 function watch() {
     browserSync.init({
         open: false,
-        proxy: "localhost:63342/htdocs/mgallagher56.github.io/app/index.html"
+        proxy: "localhost:63342/htdocs/mgallagher56.github.io/dist/index.html"
     });
-    gulp.watch('app/scss/**/*.scss', style);
-    gulp.watch('app/*.html').on('change', browserSync.reload)
-    gulp.watch('app/js/**/*.js').on('change', browserSync.reload)
+    gulp.watch('src/scss/**/*.scss', style);
+    gulp.watch('src/*.html').on('change', browserSync.reload)
+    gulp.watch('src/js/**/*.js').on('change', browserSync.reload)
 }
 
 exports.style = style;
